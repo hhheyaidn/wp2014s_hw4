dfee189e9ab9467d3e413d7c243d8327a58112c7
hhheyaidn <hhheyaidn26@gmail.com>
2014-06-05 13:55:59 +0800

    v1.0

--- js/script.js
+++ js/script.js
@@ -36,10 +36,7 @@ FB.getLoginStatus(function(response) {
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
     var img = new Image(); // 新增圖像1
     img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
-	var img2 = new Image(); //新增圖像2
-	img2.src = "img/natures.jpg" //圖像路徑
-	var img3 = new Image();//新增圖像3
-	img3.src = "img/typography.png"//圖像路徑
+	
 	
 	
 
